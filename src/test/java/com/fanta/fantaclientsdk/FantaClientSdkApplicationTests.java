0adea12ee48ea48d5278b93b6dfb79b25bddd7f4
Fanta <857035389@qq.com>
2023-05-24 23:23:48 +0800

    init

---
+++ src/test/java/com/fanta/fantaclientsdk/FantaClientSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanta.fantaclientsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantaClientSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
